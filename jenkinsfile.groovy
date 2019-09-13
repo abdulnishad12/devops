ba579788070edc69247b6ccd898f8bc6a454d401
Nishad A <abdulnishad12@gmail.com>
2019-09-13 16:15:29 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,7 +6,12 @@ pipeline {
 		echo "With nagios"
 		script{
     def body = '{"host": "web01", "duration": 600}'
- def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'GET', requestBody: patchOrg, url: "${url}"
+ def response =httpRequest (consoleLogResponseBody: true,
+      contentType: 'APPLICATION_JSON',
+      httpMode: 'GET',
+      requestBody: null,
+      url: "${url}",
+      validResponseCodes: '200')
  echo ${response}
 		}
 
